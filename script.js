2c9c9e1ea509e151155afac994bac1c329820298
theololw <theodor@norlinder.com>
2024-09-17 21:09:34 +0200

    calculator website test

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function appendToDisplay(value) {
-    document.getElementById('display').value += value;
+    document.getElementById("display").value += value;
 }
 
 function clearDisplay() {
-    document.getElementById('display').value = '';
+    document.getElementById("display").value = "";
 }
 
-function backspace() {
-    const display = document.getElementById('display');
-    display.value = display.value.slice(0, -1);
+function deleteLast() {
+    let display = document.getElementById("display").value;
+    document.getElementById("display").value = display.slice(0, -1);
 }
 
-function calculate() {
-    const display = document.getElementById('display');
+function calculateResult() {
+    let display = document.getElementById("display").value;
     try {
-        display.value = eval(display.value);
+        document.getElementById("display").value = eval(display);
     } catch (error) {
-        display.value = 'Error';
+        document.getElementById("display").value = "Error";
     }
 }
